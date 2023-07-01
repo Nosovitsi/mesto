7c663a25ad9c00e88d1c07875bfb7c3b3e81d980
Gleb Nosovitski <nosovitski.gleb@gmail.com>
2023-07-01 16:14:18 +0300

    popup wip

--- script/index.js
+++ script/index.js
@@ -4,14 +4,22 @@ const popup = document.querySelector('.popup');
   // Находим поля формы в DOM
 const nameValue = document.querySelector('.profile__name');
 const jobValue = document.querySelector('.profile__job');
-const closeEditButton = popupEdit.querySelector('.popup__close-button')
+const popupEdit = document.querySelector('.popup_edit');
+const closeEditButton = popupEdit.querySelector('.popup__close-button');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
+
     // Обработчик «отправки» формы, хотя пока
     // она никуда отправляться не будет
-    function handleFormSubmit(evt) {
-      evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+      function popupOpen() {
+        popup.classList.add('popup_opened');
+      }
+
+      function popupClose() {
+        popup.classList.remove('popup_opened');
+      }
+      // Эта строчка отменяет стандартную отправку формы.
       // Так мы можем определить свою логику отправки.
       // О том, как это делать, расскажем позже.
 
@@ -20,8 +28,12 @@ const jobInput = document.querySelector('.popup__input_type_job');
       // Выберите элементы, куда должны быть вставлены значения полей
 
       // Вставьте новые значения с помощью textContent
+        editButton.addEventListener('click', function () {
+          nameInput.value = nameValue.textContent;
+          jobInput.value = jobValue.textContent;
+          openPopup(popupEdit);
+        });
 
-    }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
