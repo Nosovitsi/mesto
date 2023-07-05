50e68565461f3fea89134735598d52d95b59a2d7
Gleb Nosovitski <nosovitski.gleb@gmail.com>
2023-07-05 22:01:59 +0300

    like add^ template error fix

--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
+import {initialCards} from "./cards.js"; 
+
 const popup = document.querySelector('.popup');
 const editBtnProfile = document.querySelector('.profile__edit-button');
 
@@ -11,86 +13,92 @@ const jobValue = document.querySelector('.profile__job');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
+const placeInput = document.querySelector('#place-input');
+const linkInput = document.querySelector('#link-input');
+
 const profileAddButton = document.querySelector('.profile__add-button');
 const addCard = document.querySelector('#add-card');
 const editCard = document.querySelector('#edit-card');
 
 /* Edit Btn */
 function popupOpenEditBtn() {
-  nameInput.value = nameValue.textContent;
-  jobInput.value = jobValue.textContent;
-  popup.classList.add('popup_opened');
+    nameInput.value = nameValue.textContent;
+    jobInput.value = jobValue.textContent;
+    popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  const openedPopup = document.querySelector('.popup_opened');
-  openedPopup.classList.remove('popup_opened');
+    const openedPopup = document.querySelector('.popup_opened');
+    openedPopup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
-  evt.preventDefault();
-  nameValue.textContent = nameInput.value;
-  jobValue.textContent = jobInput.value;
+    evt.preventDefault();
+    nameValue.textContent = nameInput.value;
+    jobValue.textContent = jobInput.value;
 
-  closePopup();
+    closePopup();
 }
 
 /* Add Btn */
 function popupOpenAddBtn(e) {
-  console.log(e)
-  addCard.classList.add('popup_opened');
+    console.log(e)
+    addCard.classList.add('popup_opened');
 }
 
 function popupCloseAddBtn() {
-  addCard.classList.remove('popup_opened');
+    addCard.classList.remove('popup_opened');
 }
 
 
 editBtnProfile.addEventListener('click', popupOpenEditBtn);
 addFormCloseButton.addEventListener('click', () => {
-  addCard.classList.remove('popup_opened');
+    addCard.classList.remove('popup_opened');
 });
 editFormCloseButton.addEventListener('click', () => {
-  editCard.classList.remove('popup_opened');
+    editCard.classList.remove('popup_opened');
 });
 
 
-/* Profile add image */
+/*Profile add image*/
 profileAddButton.addEventListener('click', popupOpenAddBtn);
 
 popupForm.addEventListener('submit', handleFormSubmit);
-
 
 const cards = document.querySelector('.cards')
 const template = document.querySelector('.template')
 
 function renderCard(card) {
-  let temp = document.getElementsByTagName("template")[0];
-  const clone = temp.content.cloneNode(true);
-  clone.querySelector('.card__image').src = card.link;
-  clone.querySelector('.card__description').textContent = card.name;
-  clone.querySelector('.card__like').addEventListener('click', function (evt) {
-    evt.target.classList.add('.card__like_active')
-  });
-  clone.querySelector('.card__delete').addEventListener('click', function (evt) {
-    const card = evt.target.closest('.card');
-    cards.removeChild(card)
-  });
-  cards.prepend(clone);
+    let temp = document.getElementsByTagName("template")[0];
+    const clone = temp.content.cloneNode(true);
+    clone.querySelector('.card__image').src = card.link;
+    clone.querySelector('.card__description').textContent = card.name;
+    clone.querySelector('.card__like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('card__like_active')
+    });
+    clone.querySelector('.card__delete').addEventListener('click', function (evt) {
+        const card = evt.target.closest('.card');
+        cards.removeChild(card)
+    });
+    cards.prepend(clone);
 }
 
 function render() {
-  initialCards.forEach((card) => {
-    renderCard(card)
-  })
+    initialCards.forEach((card) => {
+        renderCard(card)
+    })
 }
-
 render();
 
 addCard.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-  const formData = new FormData(evt.target);
-  const cardData = Object.fromEntries(formData);
-  renderCard(cardData);
-  popupCloseAddBtn();
+    evt.preventDefault();
+    const formData =  {name: placeInput.value, link: linkInput.value};
+    renderCard(formData);
+    popupCloseAddBtn();
 })
+
+/* like */
+
+const likeHeart = (event) => {
+  event.target.classList.toggle("test");
+};
