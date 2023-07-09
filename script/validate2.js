0c55f7a07453912dd8fa66b9cbf3879bc105dcef
Gleb Nosovitski <nosovitski.gleb@gmail.com>
2023-07-09 15:41:25 +0300

    fix valid

--- script/validate2.js
+++ script/validate2.js
@@ -1,23 +1,60 @@
+function showError(inputElement, errorElement) {
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.textContent = inputElement.validationMessage;
+}
+
+function hideError(inputElement, errorElement) {
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.textContent = inputElement.validationMessage;
+}
+
 
 function checkInputValidity(inputElement) {
   const isInputValid = inputElement.validity.valid;
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
   if(!isInputValid) {
-    inputElement.classList.add("popup__input_type_error");
+    showError(inputElement, errorElement);
+  }  else {
+    hideError(inputElement, errorElement);
+  }
+}
+
+function disabledButton(buttonElement) {
+  buttonElement.disabled = "disabled";
+  buttonElement.classList.add("popup__submit_inactive");
+}
+
+function enabledButton(buttonElement) {
+  buttonElement.disabled = false;
+  buttonElement.classlist.remove("popup__submit_inactive");
+}
+
+function toggleButtonState(buttonElement, isActive) {
+  if (!isActive) {
+    disabledButton(buttonElement);
+  } else {
+    enabledButton(buttonElement)
   }
 }
 
 function setEventListener(formElement){
   const inputList = formElement.querySelectorAll(".popup__input");
+  const submitButtonElement = formElement.querySelector(".popup__submit");
+
+toggleButtonState(submitButtonElement,formElement.checkInputValidity()); 
 
   [...inputList].forEach(function(inputElement){
     inputElement.addEventListener('input', function (){
-      console.log(inputElement.validity);
+      toggleButtonState(submitButtonElement, formElement.checkInputValidity());
+      checkInputValidity(inputElement, formElement);
     })
   })
+
   formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    console.log('test');
+    if (!formElement.checkValidity()) return;
+
+    console.log('Форма отправлена');
 });
 }
 
