3f6b43389efb68d87167c898f62fd5b8df4c5e94
Gleb Nosovitski <nosovitski.gleb@gmail.com>
2023-07-09 14:54:32 +0300

    input border fix color

--- script/validate2.js
+++ script/validate2.js
@@ -1,88 +1,33 @@
-function showError(inputElement, errorElement, config) {
-  inputElement.classList.add(config.inputErrorClass);
-  errorElement.textContent = inputElement.validationMessage;
-}
 
-function hideError(inputElement, errorElement, config) {
-  inputElement.classList.remove(config.inputErrorClass);
-  errorElement.textContent = inputElement.validationMessage;
-}
-
-function chekInputValidity(inputElement, formElement, config) {
-  inputElement.setCustomValidity("");
-
+function checkInputValidity(inputElement) {
   const isInputValid = inputElement.validity.valid;
-  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
-  //   console.log(inputElement.validity);
-  checkPhoneValidity(inputElement);
-  checkPasswordValidity(formElement, inputElement);
-
-  if (!isInputValid) {
-    showError(inputElement, errorElement, config);
-  } else {
-    hideError(inputElement, errorElement, config);
+  if(!isInputValid) {
+    inputElement.classList.add('popup__input_type_error');
   }
 }
 
-function disabledButton(buttonElement, config) {
-  buttonElement.disabled = "disabled";
-  buttonElement.classList.add(config.inactiveButtonClass);
-}
+function setEventListener(formElement){
+  const inputList = formElement.querySelectorAll(".popup__input");
 
-function enabledButton(buttonElement, config) {
-  buttonElement.disabled = false;
-  buttonElement.classList.remove(config.inactiveButtonClass);
-}
-
-function toggleButtonState(buttonElement, isActive, config) {
-  if (!isActive) {
-    disabledButton(buttonElement, config);
-  } else {
-    enabledButton(buttonElement, config);
-  }
-}
-
-function setEventListener(formElement, config) {
-  const inputList = formElement.querySelectorAll(config.inputSelector);
-  const submitButtonElement = formElement.querySelector(
-    config.submitButtonSelector
-  );
-
-  toggleButtonState(submitButtonElement, formElement.checkValidity(), config);
-
-  [...inputList].forEach(function (inputElement) {
-    inputElement.addEventListener("input", function () {
-      toggleButtonState(
-        submitButtonElement,
-        formElement.checkValidity(),
-        config
-      );
-      chekInputValidity(inputElement, formElement, config);
-    });
-  });
-
-  formElement.addEventListener("submit", (evt) => {
+  [...inputList].forEach(function(inputElement){
+    inputElement.addEventListener('input', function (){
+      console.log(inputElement.validity);
+    })
+  })
+  formElement.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    if (!formElement.checkValidity()) return;
-
-    console.log("Форма отправлена!");
-  });
+    console.log('test');
+});
 }
 
-function enableValidation(config) {
-  const formsList = document.querySelectorAll(config.formSelector);
 
-  [...formsList].forEach(function (formElement) {
-    setEventListener(formElement, config);
-  });
+
+function enableValidation() {
+  const formsList = document.querySelectorAll('.popup__form');
+ 
+  [...formsList].forEach(function (formElement){
+setEventListener(formElement);
+  })
 }
 
-const config = {
-  formSelector: ".form",
-  inputSelector: ".form__input",
-  submitButtonSelector: ".form__button",
-  inactiveButtonClass: "form__button_invalid",
-  inputErrorClass: "form__input_state_invalid",
-};
-
-enableValidation(config);
+enableValidation();
